25822dc91614ee3bc10860e815a10da53ac4579e
asmi221999 <40886140+asmi221999@users.noreply.github.com>
2018-07-06 15:29:36 +0530

    Create client.py

---
+++ client.py
@@ -0,0 +1,8 @@
+# asmi
+import socket
+UDP_IP_ADDRESS = "127.0.0.1"
+UDP_PORT_NO = 6777
+Message = "Hello, Server"
+bytesToSend=str.encode(Message)
+clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+clientSock.sendto(bytesToSend, (UDP_IP_ADDRESS, UDP_PORT_NO))
