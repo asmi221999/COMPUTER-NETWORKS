121075f1bcb2cd5244de1df2d998aa8d24bb3c6c
asmi221999 <40886140+asmi221999@users.noreply.github.com>
2018-10-07 13:49:44 +0530

    Create download.py

---
+++ download.py
@@ -0,0 +1,2 @@
+import urllib.request
+urllib.request.urlretrieve("https://image.shutterstock.com/image-illustration/flowers-watercolor-illustration-tender-bouquet-260nw-785987104.jpg","flower.jpg")
